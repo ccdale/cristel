cf1f3f6e27fa90baca3f485adb4efa37deecf959
Chris Allison <chris.allison@hotmail.com>
2014-07-16 05:02:17 +0100

    Created cristel.class.php - the actual application class

--- lib/cristel.class.php
+++ lib/cristel.class.php
@@ -9,22 +9,41 @@
  * chris.allison@hotmail.com
  *
  * Started: Wednesday 16 July 2014, 04:36:37
- * Last Modified: Wednesday 16 July 2014, 04:37:18
+ * Last Modified: Wednesday 16 July 2014, 05:00:58
  * Revision: $Id$
  * Version: 0.00
  */
 
 require_once "base.class.php";
+require_once "logging.class.php";
+require_once "cli.class.php"; // required by logging class
+require_once "simple_mysql.class.php";
 
 class Cristel extends Base
 {
-    public function __construct($logg=false)/*{{{*/
+    private $dbhost;
+    private $dbuser;
+    private $dbpass;
+    private $dbname;
+    public $logg;
+    public $mx;
+
+    public function __construct($dbpass,$loglevel=LOG_INFO,$toconsole=true,$logident="CRISTEL",$dbhost="localhost",$dbuser="tvd",$dbname="tv")/*{{{*/
     {
-        parent::__construct($logg);
+        $this->dbpass=$dbpass;
+        $this->dbuser=$dbuser;
+        $this->dbhost=$dbhost;
+        $this->dbname=$dbname;
+        $this->logg=new Logging($toconsole,$logident,0,$loglevel,false,false);
+        parent::__construct($this->logg);
+        $this->debug("Cristel instantiating");
+        $this->mx=new MySql($this->logg,$this->dbhost,$this->dbuser,$this->dbpass,$this->dbname);
     }/*}}}*/
     public function __destruct()/*{{{*/
     {
+        $this->mx=null;
         parent::__destruct();
+        $this->logg=null;
     }/*}}}*/
 }
 ?>
