2ddbae0b477696161163e5051f98af12aae7328a
Chris Allison <chris.allison@hotmail.com>
2015-08-02 16:23:01 +0100

    added gpl stanza

--- lib/php/simple-sqlite.class.php
+++ lib/php/simple-sqlite.class.php
@@ -6,7 +6,24 @@
  * simple-sqlite.class.php
  *
  * Started: Sunday  2 August 2015, 12:29:49
- * Last Modified: Sunday  2 August 2015, 16:20:54
+ * Last Modified: Sunday  2 August 2015, 16:22:47
+ * 
+ * Copyright (c) 2015 Chris Allison chris.allison@hotmail.com
+ *
+ * This file is part of cristel.
+ * 
+ * cristel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * cristel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with cristel.  If not, see <http://www.gnu.org/licenses/>.
  */
 require_once "base.class.php"
 
