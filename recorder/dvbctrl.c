bc6431c224e1fee0d995f3ac78161a0e8b53985f
Chris Allison <chris.charles.allison@gmail.com>
2016-09-18 11:08:41 +0100

    fix count of num data lines received

--- recorder/dvbctrl.c
+++ recorder/dvbctrl.c
@@ -7,7 +7,7 @@
  * chris.allison@hotmail.com
  *
  * Started: Sunday 27 July 2014, 06:07:48
- * Last Modified: Sunday 18 September 2016, 09:57:58
+ * Last Modified: Sunday 18 September 2016, 11:08:22
  *
  * Copyright (c) 2014 Chris Allison chris.allison@hotmail.com
  *
@@ -202,7 +202,6 @@ int rcvData(void)/*{{{*/
                             SD.errmsg=seperator+1;
                         }
                         SD.errornumber=atoi(start);
-                        numlines++;
                     }
                     break;
                 }else{
